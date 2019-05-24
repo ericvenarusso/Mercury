6162d262159b11475966e82dd49e0bce91b130dd
Eric Venarusso <eric.venarusso-ext@viavarejo.com.br>
2019-05-24 08:37:57 -0300

    circleci and configs

--- config.sh
+++ config.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Downloading Data
-wget /data/iris.csv https://pkgstore.datahub.io/machine-learning/iris/iris_csv/data/8bce8766530bf404228ea3fc026dfee3/iris_csv.csv
+wget -P data/ https://pkgstore.datahub.io/machine-learning/iris/iris_csv/data/8bce8766530bf404228ea3fc026dfee3/iris_csv.csv
+
+# Change file name
+mv data/iris_csv.csv data/iris.csv
